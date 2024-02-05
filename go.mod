cf94c017c66ae1c8fd699396b4222012c0e436f3
dimons2006@yandex.ru <diomns2006@yandex.ru>
2024-02-06 01:00:17 +0300

    Add new func in pars_expression.go

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module AEC
 go 1.21
 
 require (
+	github.com/Knetic/govaluate v3.0.0+incompatible // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
--- internal/orchestrator/services/pars_expression.go
+++ internal/orchestrator/services/pars_expression.go
@@ -1,1 +1,74 @@
 package services
+
+import (
+	"fmt"
+	"github.com/Knetic/govaluate"
+	"strings"
+	"sync"
+)
+
+func start(val string) string {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		for _, subexpression := range findSubexpressions(val) {
+			calRes, err := calculation(subexpression)
+			if err != nil {
+				fmt.Println(err)
+			}
+			val = strings.Replace(val, calRes[1], calRes[0], 1)
+		}
+	}()
+
+	wg.Wait()
+
+	return val
+}
+
+func findSubexpressions(expression string) []string {
+	var subexpressions []string
+	stack := 0
+	start := -1
+
+	for i, char := range expression {
+		if char == '(' {
+			if stack == 0 {
+				start = i
+			}
+			stack++
+		} else if char == ')' {
+			stack--
+			if stack == 0 && start != -1 {
+				subexpression := expression[start+1 : i]
+				subexpressions = append(subexpressions, subexpression)
+				start = -1
+			}
+		}
+	}
+
+	return subexpressions
+}
+
+func calculation(data string) ([]string, error) {
+	fmt.Println(data)
+	res := make([]string, 2)
+	expression, err := govaluate.NewEvaluableExpression(data)
+	if err != nil {
+		fmt.Println("Ошибка при создании выражения:", err)
+		return nil, err
+	}
+
+	result, err := expression.Evaluate(nil)
+	if err != nil {
+		fmt.Println("Ошибка при вычислении выражения:", err)
+		return nil, err
+	}
+
+	res[0] = fmt.Sprintf("%v", result)
+	res[1] = data
+
+	return res, nil
+}
